8d29ed923031f92d8075470ad3a7d67a8616a7ff
Pyotr Vasilev <vahpetr@gmail.com>
2017-03-20 11:36:32 +0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Marvin
+
+[Marvin. Don't-panic challenge](https://www.codingame.com/ide/puzzle/don't-panic)
---
+++ builder.js
@@ -0,0 +1,62 @@
+var UglifyJS = require("uglify-js");
+
+var config = {
+    fromString: true,
+    mangle: {
+        except: ['readline', 'print']
+    },
+    //compress: true,
+    //sourceMap: true,
+    //sourceMapIncludeSources: true,
+    //sourceMapName: 'js/app.min.js.map',
+    //mangle: {
+    //    //toplevel: true,
+    //    //screw_ie8: true
+
+    //},
+    compress: {
+        //screw_ie8: true, // ?
+        sequences: true, // join consecutive statemets with the “comma operator”
+        properties: false, // optimize property access: a["foo"] → a.foo
+        dead_code: true, // discard unreachable code
+        drop_debugger: true, // discard “debugger” statements
+        unsafe: true, // some unsafe optimizations (see below)
+        conditionals: true, // optimize if-s and conditional expressions
+        comparisons: true, // optimize comparisons
+        evaluate: true, // evaluate constant expressions
+        booleans: true, // optimize boolean expressions
+        loops: true, // optimize loops
+        unused: true, // drop unused variables/functions
+        hoist_funs: true, // hoist function declarations
+        hoist_vars: true, // hoist variable declarations
+        if_return: true, // optimize if-s followed by return/continue
+        join_vars: true, // join var declarations
+        cascade: true, // try to cascade `right` into `left` in sequences
+        side_effects: true, // drop side-effect-free statements
+        warnings: false, // warn about potentially dangerous optimizations/code
+        //negate_iife: true,
+        drop_console: true,
+        //droop func
+        pure_funcs: [
+            'printErr'
+        ],
+        // global definitions
+        global_defs: {
+            DEBUG: false
+        }
+    }
+};
+
+var fs = require('fs');
+fs.readFile('./index.js', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    data = 'function test(readline, print) {' + data + '}';
+    var result = UglifyJS.minify(data, config);
+    var code = result.code.replace(/^function test\(readline,print\){/, '').replace(/}$/, '');
+    fs.writeFile('marvin.js', code, (err) => {
+        if (err) throw err;
+        console.log(`Marvin update! New size ${code.length}`);
+    });
+});
