8d7d94e6ade5c25f7e10075d5869b89f0aa52632
Pyotr Vasilev <vahpetr@gmail.com>
2017-03-20 11:44:53 +0500

    rename pk to packet in comment

--- index.js
+++ index.js
@@ -6,13 +6,13 @@
 var packet = readline().split(' ');
 printErr(packet);
 
-// var nbFloors = +pk[0]; // number of floors
-// var width = +pk[1]; // width of the area
-// var nbRounds = +pk[2]; // maximum number of rounds
-// var exitFloor = +pk[3]; // floor on which the exit is found
+// var nbFloors = +packet[0]; // number of floors
+// var width = +packet[1]; // width of the area
+// var nbRounds = +packet[2]; // maximum number of rounds
+// var exitFloor = +packet[3]; // floor on which the exit is found
 var exitPos = +packet[4]; // position of the exit on its floor
-// var nbTotalClones = +pk[5]; // number of generated clones
-// var nbAdditionalElevators = +pk[6]; // ignore (always zero)
+// var nbTotalClones = +packet[5]; // number of generated clones
+// var nbAdditionalElevators = +packet[6]; // ignore (always zero)
 var elevatorsCount = +packet[7]; // number of elevators
 var exits = [];
 exits[+packet[3]] = [exitPos];
@@ -28,11 +28,11 @@ for (var i = 0; i < +packet[7]; i++) {
 while (true) {
     packet = readline().split(' ');
 
-    // var clonePos = +pk[1]; // position of the leading clone on its floor
-    // var cloneFloor = +pk[0]; // floor of the leading clone
+    // var clonePos = +packet[1]; // position of the leading clone on its floor
+    // var cloneFloor = +packet[0]; // floor of the leading clone
     exitPos = exits[+packet[0]];
 
     // var needDirection = exitPos - clonePos > 0 ? "RIGHT" : "LEFT";
-    // var direction = pk[2]; // direction of the leading clone: LEFT or RIGHT
+    // var direction = packet[2]; // direction of the leading clone: LEFT or RIGHT
     print((exitPos - +packet[1] > 0 ? "RIGHT" : "LEFT") == packet[2] ? 'WAIT' : (exitPos == +packet[1] ? "WAIT" : "BLOCK"));
 }
